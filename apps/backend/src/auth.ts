467148eac3f37a274e4d7680d6fe111a59d3074a
Aurellya <h1101241043@student.untan.ac.id>
2026-03-29 20:17:43 +0700

    mono

--- apps/backend/src/auth.ts
+++ apps/backend/src/auth.ts
@@ -1,11 +1,13 @@
 import { google } from "googleapis";
 
 export function createOAuthClient() {
-  console.log("REDIRECT:", process.env.GOOGLE_REDIRECT_URI);
+  const REDIRECT_URI = "https://monorepo-be-flame.vercel.app/auth/callback";
+  
+  console.log("MANUAL REDIRECT:", REDIRECT_URI);
   return new google.auth.OAuth2(
     process.env.GOOGLE_CLIENT_ID,
     process.env.GOOGLE_CLIENT_SECRET,
-    process.env.GOOGLE_REDIRECT_URI
+    REDIRECT_URI
   );
 }
 
